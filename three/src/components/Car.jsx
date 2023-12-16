33338c805b6aab4df8755dacacea22220ba8b9fa
dnwn1211 <dnwn3027@gmail.com>
2023-12-16 23:55:34 +0900

    threeDcar
    end

--- three/src/components/Car.jsx
+++ three/src/components/Car.jsx
@@ -5,22 +5,24 @@ import { useBox, useRaycastVehicle } from "@react-three/cannon";
 import { WheelDebug } from './WheelDebug';
 import { useWheels } from './useWheels';
 import { useControls } from './useControls';
+import { Vector3, Quaternion } from "three"; // 'THREE'를 import
 
-export function Car(){
+export function Car({ thirdPerson }) {
     let mesh = useLoader(
         GLTFLoader,
-        process.env.PUBLIC_URL+"/models/car.glb"
+        process.env.PUBLIC_URL + "/models/car.glb"
     ).scene;
 
-    const position = [-1.5,0.5,3];
+    const position = [-1.5, 0.5, 3];
     const width = 0.15;
     const height = 0.07;
     const front = 0.15;
     const wheelRadius = 0.05;
 
-    const chassisBodyArgs= [width, height, front *2];
-    const [chassisBody, chassisApi]=useBox(
-        ()=>({
+    const chassisBodyArgs = [width, height, front * 2];
+    const [chassisBody, chassisApi] = useBox(
+        () => ({
+            allowsleep: false,
             args: chassisBodyArgs,
             mass: 150,
             position,
@@ -28,10 +30,10 @@ export function Car(){
         useRef(null),
     );
 
-    const [wheels, wheelInfos]= useWheels(width, height, front, wheelRadius);
+    const [wheels, wheelInfos] = useWheels(width, height, front, wheelRadius);
 
     const [vehicle, vehicleApi] = useRaycastVehicle(
-        ()=>({
+        () => ({
             chassisBody,
             wheelInfos,
             wheels,
@@ -39,25 +41,50 @@ export function Car(){
         useRef(null),
     );
 
-    useControls(vehicleApi,chassisApi);
+    useControls(vehicleApi, chassisApi);
 
-    useEffect(()=>{
+    useFrame((state) => {
+        if (!thirdPerson) return;
+
+        let position = new Vector3(0, 0, 0);
+        position.setFromMatrixPosition(chassisBody.current.matrixWorld);
+
+        let quaternion = new Quaternion(0, 0, 0, 0);
+        quaternion.setFromRotationMatrix(chassisBody.current.matrixWorld);
+
+        let wDir = new Vector3(0, 0, 1);
+        wDir.applyQuaternion(quaternion);
+        wDir.normalize();
+
+        // camera z위치의 position 위쪽에서 비스듬히
+        let cameraPosition = position.clone().add(
+            wDir.clone().multiplyScalar(1).add(
+                new Vector3(0, 0.3, 0)
+            )
+        );
+        state.camera.position.copy(cameraPosition);
+        state.camera.lookAt(position);
+    });
+
+    useEffect(() => {
         mesh.scale.set(0.0012, 0.0012, 0.0012);
-        mesh.children[0].position.set(-365,-18,-67);
-    },[mesh]);
+        mesh.children[0].position.set(-365, -18, -67);
+    }, [mesh]);
 
-    return(
+    return (
         <group ref={vehicle} name="vehicle">
-            <mesh ref={chassisBody}>
+            <group ref={chassisBody} name='chassisBody'>
+                <primitive object={mesh} rotation-y={Math.PI} position={[0, -0.09, 0]} />
+            </group>
+            {/* <mesh ref={chassisBody}>
                 <meshBasicMaterial transparent={true} opacity={0.3}/>
                 <boxGeometry args={chassisBodyArgs}/>
-                {/* <primitive object={mesh} rotation-y={Math.PI}/> */}
-            </mesh>
-
-            <WheelDebug wheelRef={wheels[0]} radius={wheelRadius}/>
-            <WheelDebug wheelRef={wheels[1]} radius={wheelRadius}/>
-            <WheelDebug wheelRef={wheels[2]} radius={wheelRadius}/>
-            <WheelDebug wheelRef={wheels[3]} radius={wheelRadius}/>
+                <primitive object={mesh} rotationY={Math.PI}/>
+            </mesh> */}
+            <WheelDebug wheelRef={wheels[0]} radius={wheelRadius} />
+            <WheelDebug wheelRef={wheels[1]} radius={wheelRadius} />
+            <WheelDebug wheelRef={wheels[2]} radius={wheelRadius} />
+            <WheelDebug wheelRef={wheels[3]} radius={wheelRadius} />
         </group>
     );
 }
--- three/src/components/Scene.jsx
+++ three/src/components/Scene.jsx
@@ -1,23 +1,44 @@
-import{
+import {
     Environment,
     OrbitControls,
     PerspectiveCamera,
 } from "@react-three/drei";
-import {Suspense}from "react";
-import { Track }from "./Track";
-import { Ground }from "./Ground";
-import { Car }from "./Car";
+import { Suspense, useEffect, useState } from "react";
+import { Car } from "./Car";
+import { Ground } from "./Ground";
+import { Track } from "./Track";
 
-export function Scene(){
-    return(
-        <Suspense fallback={null}>
-            <Environment files={process.env.PUBLIC_URL+"/textures/envmap.hdr"} background={"both"}/>
-            <PerspectiveCamera makeDefault position={[-6,3.9,6.21]} fov={40}/>
-            <OrbitControls target={[-2.64,-0.71,0.03]}/>
+export function Scene() {
+    const [thirdPerson, setThirdPerson] = useState(false);
+    const [cameraPosition, setCameraPosition] = useState([-6, 3.9, 6.21]);
+    useEffect(() => {
+    function keydownHandler(e) {
+        if (e.key == "k") {
+          // random is necessary to trigger a state change
+          if(thirdPerson) setCameraPosition([-6, 3.9, 6.21 + Math.random() * 0.01]);
+        setThirdPerson(!thirdPerson); 
+        }
+    }
 
-            <Track/>
-            <Ground/>
-            <Car/>
-        </Suspense>
-    );
-}export default Scene;
+    window.addEventListener("keydown", keydownHandler);
+    return () => window.removeEventListener("keydown", keydownHandler);
+    }, [thirdPerson]);
+
+    return (
+    <Suspense fallback={null}>
+        <Environment
+        files={process.env.PUBLIC_URL + "/textures/envmap.hdr"}
+        background={"both"}
+        />
+
+        <PerspectiveCamera makeDefault position={cameraPosition} fov={40} />
+        {!thirdPerson && (
+        <OrbitControls target={[-2.64, -0.71, 0.03]} />
+        )}
+
+        <Ground />
+        <Track />
+        <Car thirdPerson={thirdPerson} />
+    </Suspense>
+    ); 
+}
